0d9337c833b13b3d03d73d3dd354c5e4520a5a45
Michael Kurzak <mkurzakdev@gmail.com>
2021-01-09 15:07:38 +0100

    add structure table to class Library.

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -4,13 +4,15 @@ import pl.javastart.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.7";
 
-        Book book1 = new Book("W pustyni i w puszczy","Henryk Siękiewicz",
+
+        Book[] books = new Book[1000];
+        books[0] = new Book("W pustyni i w puszczy","Henryk Siękiewicz",
                 2010, 296, "W pustyni i w puszczy", "8446544684846846");
-        Book book2 = new Book("Java. Efektywne programowamie. Wydanie II","Joshua Bolch",
+        books[1] = new Book("Java. Efektywne programowamie. Wydanie II","Joshua Bolch",
                 2009, 352, "Helion", "9788324620845");
-        Book book3 = new Book("SCJP Sun Certyfified Programmer for Java 6 Study Guide",
+        books[2] = new Book("SCJP Sun Certyfified Programmer for Java 6 Study Guide",
                 "Bert Bates, Katherine Sierra",2008,
                 851, "McGraw-Hill Osborne Media");
 
@@ -18,9 +20,9 @@ class Library {
 
         System.out.println(appName);
         System.out.println("Książki sotępne w bibliotece");
-        book1.printInfo ();
-        book2.printInfo ();
-        book3.printInfo ();
+        books[0].printInfo ();
+        books[1].printInfo ();
+        books[2].printInfo ();
 
 
     }
