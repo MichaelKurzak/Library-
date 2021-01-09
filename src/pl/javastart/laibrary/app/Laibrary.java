b159043852c1517c8058bece4610363e75d7362b
Michael Kurzak <mkurzakdev@gmail.com>
2021-01-09 12:52:48 +0100

    copy files

---
+++ src/pl/javastart/laibrary/app/Laibrary.java
@@ -0,0 +1,27 @@
+package pl.javastart.laibrary.app;
+
+import pl.javastart.laibrary.model.Book;
+
+class Laibrary {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.5";
+
+        Book book1 = new Book("W pustyni i w puszczy","Henryk Siękiewicz",
+                2010, 296, "W pustyni i w puszczy", "8446544684846846");
+        Book book2 = new Book("Java. Efektywne programowamie. Wydanie II","Joshua Bolch",
+                2009, 352, "Helion", "9788324620845");
+        Book book3 = new Book("SCJP Sun Certyfified Programmer for Java 6 Study Guide",
+                "Bert Bates, Katherine Sierra",2008,
+                851, "McGraw-Hill Osborne Media");
+
+
+
+        System.out.println(appName);
+        System.out.println("Książki sotępne w bibliotece");
+        book1.printInfo ();
+        book2.printInfo ();
+        book3.printInfo ();
+
+
+    }
+}
---
+++ src/pl/javastart/laibrary/model/Book.java
@@ -0,0 +1,78 @@
+package pl.javastart.laibrary.model;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int releaseData;
+    private int pages;
+    private String publisher ;
+    private String isbn;
+
+    public Book(String title, String author, int releaseData, int pages, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.releaseData = releaseData;
+        this.pages = pages;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String author, int releaseData, int pages, String publisher, String isbn) {
+        this(title, author, releaseData, pages, publisher);
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseData() {
+        return releaseData;
+    }
+
+    public void setReleaseData(int releaseData) {
+        this.releaseData = releaseData;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void printInfo() {
+        String info = title + "; " + author + "; " + releaseData + "; " + pages + "; " + publisher + "; " + isbn;
+        
+        System.out.println(info);
+    }
+}
