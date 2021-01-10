c75ac3557a2b89c08f32afa4daeea9c132e0099f
Michael Kurzak <mkurzakdev@gmail.com>
2021-01-10 19:30:26 +0100

    add class DataReader.java in said functions scanner to add books.

---
+++ src/pl/javastart/io/DataReader.java
@@ -0,0 +1,33 @@
+package pl.javastart.io;
+
+import pl.javastart.library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public Book readAndCreateBook() {
+        System.out.println("Proszę o podanie tytułu");
+        String title = sc.nextLine();
+        System.out.println("Proszę o podanie autora");
+        String author = sc.nextLine();
+        System.out.println("Proszę o podanie wydawcy");
+        String publisher = sc.nextLine();
+        System.out.println("Proszę o podanie ISBN");
+        String isbn = sc.nextLine();
+        System.out.println("Proszę o podanie daty wydania");
+        int releaseData = sc.nextInt();
+        System.out.println("Proszę o podanie ilości stron");
+        int pages = sc.nextInt();
+        sc.nextLine();
+        return new Book (title, author, releaseData, pages, publisher, isbn);
+
+    }
+
+    public void clos() {
+        sc.close();
+    }
+
+}
--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,29 +1,23 @@
 package pl.javastart.library.app;
 
+import pl.javastart.io.DataReader;
 import pl.javastart.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.7";
-
+        final String appName = "Biblioteka v0.8";
 
         Book[] books = new Book[1000];
-        books[0] = new Book("W pustyni i w puszczy","Henryk Siękiewicz",
-                2010, 296, "W pustyni i w puszczy", "8446544684846846");
-        books[1] = new Book("Java. Efektywne programowamie. Wydanie II","Joshua Bolch",
-                2009, 352, "Helion", "9788324620845");
-        books[2] = new Book("SCJP Sun Certyfified Programmer for Java 6 Study Guide",
-                "Bert Bates, Katherine Sierra",2008,
-                851, "McGraw-Hill Osborne Media");
 
-
+        DataReader dataReader = new DataReader();
+        books [0] = dataReader.readAndCreateBook();
+        books [1] = dataReader.readAndCreateBook();
+        dataReader.clos();
 
         System.out.println(appName);
         System.out.println("Książki sotępne w bibliotece");
         books[0].printInfo ();
         books[1].printInfo ();
-        books[2].printInfo ();
-
-
     }
+
 }
